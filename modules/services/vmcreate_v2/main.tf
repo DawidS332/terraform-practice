e4d777d9e13894cd5cfd182881ccdf2a9e657bf0
Dawid Strzelczyk <dawid.strzelczyk@fedex.com>
2021-01-14 10:44:31 +0000

    added providers/tf

--- modules/services/vmcreate_v2/main.tf
+++ modules/services/vmcreate_v2/main.tf
@@ -1,12 +1,3 @@
-# Configure the Azure Providers
-
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.41.0"
-  features {}
-}
-
-
 resource "azurerm_resource_group" "plexrg" {
   name = "${var.vm_name}-rsg"
   location = "North Europe"
